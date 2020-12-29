e21b00ff2c4bd06122cb078ee52572a4e3ea8b72
Frederic COLLONVAL <fcollonval@gmail.com>
2020-12-29 12:55:04 +0100

    Try fixing style duplication

--- style/index.js
+++ style/index.js
@@ -1,7 +1,2 @@
-import '@lumino/widgets/style/index.css';
-import '@jupyterlab/ui-components/style/index.css';
-import '@jupyterlab/apputils/style/index.css';
-import '@jupyterlab/application/style/index.css';
-
 import './base.css';
 import './table.css';
